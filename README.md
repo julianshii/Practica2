453b9ad4176e13324c59dc72a11ae02b39c9e9cf
Julian Gabet <juliangabet@gmail.com>
2025-03-30 20:27:51 -0300

    Agrego src y el main de jupyter notebook. Actualizo el README.md

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Practica2
+para ejecutar este programa es requerido instalar las librerias necesarias para el proyecto con este comando:\
+pip install -r requirements.txt
+ejecuta este comando en tu gitbash y ya tendras las dependencias necesarias para correr este programa, en este caso, se usara unicamente jupyter notebook.
+
+Julian Gabet 25597/0
---
+++ src/Ranking.py
@@ -0,0 +1,55 @@
+#Funcion para calcular los puntos de un jugador
+def calcular_puntos(kills, assists, deaths):
+    return (kills * 3) + (assists) - (deaths)
+#Funcion para calcular el MVP de una ronda
+def calcular_mvp(ronda):
+    mvp = None
+    max_puntos = -1
+    for jugador, stats in ronda.items():
+        kills = stats['kills']
+        assists = stats['assists']
+        deaths = stats['deaths']
+        puntos = calcular_puntos(kills, assists, deaths)
+        if puntos > max_puntos:
+            max_puntos = puntos
+            mvp = jugador
+    return mvp
+#incializar el diccionario de jugadores
+def inicializar_jugadores():
+    jugadores = {}
+    for jugador in ['Shadow', 'Blaze', 'Viper', 'Frost', 'Reaper']:
+        jugadores[jugador] = {
+            'kills': 0,
+            'assists': 0,
+            'deaths': 0,
+            'mvp_count': 0,
+            'total_points': 0
+        }
+    return jugadores
+
+#Actualizar el diccionario de jugadores con los resultados de una ronda
+def actualizar_jugadores(jugadores, ronda):
+    for jugador, stats in ronda.items():
+        jugadores[jugador]['kills'] += stats['kills']
+        jugadores[jugador]['assists'] += stats['assists']
+        jugadores[jugador]['deaths'] += int(stats['deaths'])
+        if jugador == calcular_mvp(ronda):
+            jugadores[jugador]['mvp_count'] += 1
+        jugadores[jugador]['total_points'] = calcular_puntos(jugadores[jugador]['kills'], jugadores[jugador]['assists'], jugadores[jugador]['deaths'])
+    return jugadores
+
+
+
+#Programa principal
+def Ranking(rounds):
+    jugadores = inicializar_jugadores()
+    for i, ronda in enumerate(rounds):
+        jugadores = actualizar_jugadores(jugadores, ronda)
+        if i == 4:
+            print("Ranking final:")
+        else: print(f"\nResultados de la ronda {i + 1}:")
+        print("Jugador      Kills      Asistencias      Muertes      MVPs      Puntos")
+        print("------------------------------------------------------------------------------")
+        for jugador, stats in jugadores.items():
+            print(jugador,"      ",stats['kills'],"             ", stats['assists'],"          ", stats['deaths'],"          ", stats['mvp_count'],"         ", stats['total_points'])
+        print("------------------------------------------------------------------------------")
